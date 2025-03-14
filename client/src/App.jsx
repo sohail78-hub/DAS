e87ea2ac3bdf791d0185cb8326380c82b286d5dd
HammadRaza <01hammadraza@gmail.com>
2025-03-15 01:14:58 +0500

    'here'

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import HeroSection from './components/Hero'
+import ServicesSection from './components/Services'
 
 const App = () => {
   return (
     <>
       <Navbar/>
       <HeroSection/>
+      <ServicesSection/>
     </>
   )
 }
